9438887977dd9f9ccd92fb7f3a66a52d5287f1ce
shivanshu11092003 <sshivanshugupta11@gmail.com>
2025-10-14 23:24:31 +0530

    feat: wrong spell

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,15 +2,15 @@ import { useAuth } from "@/Hooks/Auth";
 import { createFileRoute, Navigate } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
-  component: App,
+	component: App,
 });
 
 function App() {
-  const auth = useAuth();
-  if (auth?.isAuthenticated) return <Navigate to="/user" />;
-  return (
-    <>
-      <button onClick={auth?.login}>Login</button>
-    </>
-  );
+	const auth = useAuth();
+	if (auth?.isAuthenticated) return <Navigate to="/user" />;
+	return (
+		<>
+			<button onClick={auth?.login}>Login with Diable buton</button>
+		</>
+	);
 }
