bc9320d41c62a0996157f5696511a0c21ee355e2
shivanshu11092003 <sshivanshugupta11@gmail.com>
2025-10-18 01:39:07 +0530

    CI/CD fix

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,19 +7,26 @@ export const Route = createFileRoute("/")({
 
 function App() {
 	const auth = useAuth();
-	// This funciton definitly needs to be intialized before acess.
-	function recieveDataFromEnviroment() {
-		console.log("Teh goverment API responsed sucessfully!");
+	// This funciton definitly needs to be initialized before acess.
+	function receiveDataFromEnviroment() {
+		console.log("Teh goverment API responsed successfully!");
 	}
 
 	if (auth?.isAuthenticated) return <Navigate to="/user" />;
 	return (
 		<>
 			{/* <button onClick={auth?.login}>Login with Diable buton</button> */}
-			<p>Teh goverment API responsed sucessfully! </p>
-			<p>Teh goverment API responsed sucessfully! </p>
-			<p>Teh goverment API responsed sucessfully! </p>
-			<p>Teh goverment API responsed sucessfully! </p>
+			<p>Teh goverment API responsed successfully! </p>
+			<p>Teh goverment API responsed successfully! </p>
+			<p>Teh goverment API responsed successfully! </p>
+			<p>Teh goverment API responsed successfully! </p>
+			<p>
+				Ths projct aimz to improove the qulity of dat colectd from varous
+				sourcess. By usng advnced algoritms, we can analize the infomation more
+				acurately and genrate bettr resultss. Evry moduel is designd to be
+				flexble and effcient, making the systm esy to maintane and updte ovr
+				time.
+			</p>
 		</>
 	);
 }
